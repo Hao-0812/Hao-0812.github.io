249795645bfd1002c31bf8fa9e31323223ccf647
zhonghao <17384790449@163.com>
2024-03-06 15:26:10 +0800

    Site updated: 2024-03-06 15:26:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/06/Hexo搭建个人网站/","2024/03/05/hello-world/","2024/03/06/paper/","2024/03/06/Hexo主题设置/"];function toRandomPost(){
+var posts=["2024/03/06/CMake基础/","2024/03/06/Hexo主题设置/","2024/03/06/Hexo搭建个人网站/","2024/03/06/Makefile基础/","2024/03/06/VTK基础/","2024/03/06/Linux基础/","2024/03/05/hello-world/","2024/03/06/paper/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
