6bdd663f66fa788e82ab6a92617e1ead6258e1df
zhonghao <17384790449@163.com>
2024-03-27 16:18:10 +0800

    Site updated: 2024-03-27 16:18:08

--- js/card_love.js
+++ js/card_love.js
@@ -1,6 +1,13 @@
 var htmer_time = document.getElementById("htmer_time");
 var htmer_time_time = null;
-nction setTime() {
+
+function isLeapYear(year) {
+    // 如果年份是400的倍数，那么是闰年
+    // 如果年份是4的倍数但不是100的倍数，也是闰年
+    return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
+}
+
+function setTime() {
     var create_time = Math.round(
         // 这里是你们在一起的时间，注意月份要减一
         new Date(Date.UTC(2017, 7, 12, 17, 0, 0)).getTime() / 1000
@@ -9,6 +16,8 @@ nction setTime() {
         (new Date().getTime() + 8 * 60 * 60 * 1000) / 1000
     );
     currentTime = secondToDate(timestamp - create_time);
+    var day = currentTime[0] * (isLeapYear(currentTime[0]) ? 366 : 365) + currentTime[1];
+    currentDay = secondToDay(timestamp - create_time);
     currentTimeHtml =
         currentTime[0] +
         ' 年 ' +
@@ -19,9 +28,24 @@ nction setTime() {
         currentTime[3] +
         ' 分 ' +
         currentTime[4] +
-        ' 秒 ';
+        ' 秒 ' +
+        day +
+        ' 天 ';
     htmer_time.innerHTML = currentTimeHtml;
 }
+
+// function setDay() {
+//     var create_time = Math.round(
+//         // 这里是你们在一起的时间，注意月份要减一
+//         new Date(Date.UTC(2017, 7, 12, 17, 0, 0)).getTime() / 1000
+//     );
+//     var timestamp = Math.round(
+//         (new Date().getTime() + 8 * 60 * 60 * 1000) / 1000
+//     );
+//     currentDay = secondToDay(timestamp - create_time);
+//     currentTimeHtml = currentDay[0] + ' 天 ';
+//     htmer_time.innerHTML = currentTimeHtml;
+// }
 
 function secondToDate(second) {
     if (!second) {
@@ -50,10 +74,19 @@ function secondToDate(second) {
     return time;
 }
 
+function secondToDay(second) {
+    if (!second) {
+        return 0;
+    }
+    var day = 0;
+    day = parseInt(second / (24 * 3600));
+    return day;
+}
+
 function cheesy_pick_up_lines() {
     let html = "";
     for (let i = 0; i < 1; i++) {
-      fetch("https://api.vvhan.com/api/love")
+      fetch("一往而深❤")
         .then((data) => data.text())
         .then((data) => {
           html += "<li>" + data + "</li>";
