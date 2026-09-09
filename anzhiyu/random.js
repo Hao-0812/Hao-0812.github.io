1b96d712827dba8101ef80d39f48d44aa99ea2cb
haozhong <17384790449@163.com>
2026-09-09 23:46:32 +0800

    Site updated: 2026-09-09 23:46:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/06/CMake基础/","2024/03/06/Linux基础/","2024/03/06/Hexo搭建个人网站/","2026/08/30/hello-world/"];function toRandomPost(){
+var posts=["2024/03/06/CMake基础/","2024/03/06/Linux基础/","2024/03/06/Hexo搭建个人网站/","2026/08/30/hello-world/","2026/09/09/Noise/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
